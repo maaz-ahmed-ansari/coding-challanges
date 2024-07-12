ff665153796c77950de7782bf05879ef13f47571
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2024-07-12 23:17:26 +0530

    Differene

--- scripts/easy/01.find-the-difference.py
+++ scripts/easy/01.find-the-difference.py
@@ -1,19 +1,40 @@
-class Solution(object):
-    def findTheDifference(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+def findTheDifference(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: str
+    """
 
-        diff = 0
-        for char_t in t:
-            diff += ord(char_t)
-        
-        for char_s in s:
-            diff -= ord(char_s)
-        
-        return chr(diff)
+    # Approach 1 - Using Dictionary --------------
+    map = dict()
+
+    for i in s:
+        if i not in map:
+            map[i] = 1
+        else:
+            map[i] += 1
+    
+    for char in t:
+        if char not in map:
+            return char
+
+        map[char] -= 1
+
+        if map[char] < 0:
+            return char
+
+
+    # Approach - 2 Using Unicode conversion --------------
+
+    diff = 0
+
+    for char in t:
+        diff += ord(char)
+    
+    for char in s:
+        diff -= ord(char)
+    
+    return chr(diff)
     
 # The ord() function returns the number representing the unicode code of a specified character.
 # The chr() function returns the character that represents the specified unicode.
