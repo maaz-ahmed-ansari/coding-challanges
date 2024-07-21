c52713a8a41b2d99ae61d25f8898766f95d21744
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2024-07-21 11:10:09 +0530

    Remove dups from sorted array

--- scripts/easy/06.remove-duplicates-from-sorted-array.py
+++ scripts/easy/06.remove-duplicates-from-sorted-array.py
@@ -1,20 +1,27 @@
-def removeDuplicates(nums):
+def removeDuplicates(self, nums):
     """
     :type nums: List[int]
     :rtype: int
     """
-    len_nums = len(nums)
-    if len_nums == 1:
+    # get len(nums)
+    nums_len = len(nums)
+    # if size of nums is 1 --> return 1 as only 1 element
+    if nums_len == 1:
         return 1
     
+    # else iterate over nums using two pointers
+    # initialize fist pointer at 0
     i = 0
-    j = 1
-    while j < len_nums:
+    # iterate over nums starting with 1 index till end  --> this is second pointer  
+    for j in range(1, nums_len):
+        # compare if second pointer content is > first pointers content
         if nums[j] <= nums[i]:
-            j += 1
+            # if not greater than, go to next element using 2nd pointer
+            continue
+        # if greater then increase first pointer and assign it with values of second pointer's content
         else:
             i += 1
             nums[i] = nums[j]
-            j += 1
+        # and go to next element and repeat untill last element reached in 2nd pointer
     
     return i + 1
